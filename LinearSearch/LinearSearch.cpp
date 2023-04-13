98238b0192f5c75a7938a3afd819090d505c3638
emzhran <zachran750@gmail.com>
2023-04-13 14:41:51 +0700

    tambahan void input

--- LinearSearch/LinearSearch.cpp
+++ LinearSearch/LinearSearch.cpp
@@ -4,3 +4,24 @@ using namespace std;
 int arr[20];
 int n;
 int i;
+
+void input() {
+	while (true)
+	{
+		cout << "Enter the number of elements in the array : ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
+	}
+
+	cout << "\n---------------------\n";
+	cout << " Enter array elements \n";
+	cout << "\n---------------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i < 1) << "> ";
+		cin >> arr[i];
+	}
+}
